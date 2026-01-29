4d85ca7f8d3de8af209b6b162730b072ef04b9ee
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-01-29 16:16:04 +0200

    Aristova N. Homework 5

--- homework/natalia_aristova/Homework_5/lesson_5_hw.py
+++ homework/natalia_aristova/Homework_5/lesson_5_hw.py
@@ -9,9 +9,9 @@ a = 'результат операции: 42'
 b = 'результат операции: 514'
 c = 'результат работы программы: 9'
 
-print(int(a[(a.index(':')+2):]) + 10)
-print(int(b[(b.index(':')+2):]) + 10)
-print(int(c[(c.index(':')+2):]) + 10)
+print(int(a[(a.index(':') + 2):]) + 10)
+print(int(b[(b.index(':') + 2):]) + 10)
+print(int(c[(c.index(':') + 2):]) + 10)
 
 # Task 3
 
