de0c58c6bc9c7fd0db7b121be57e7fe1f99df6cb
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-02-04 19:54:35 +0200

    Aristova N. Homework 7

--- homework/natalia_aristova/Homework_7/lesson_7_hw_3.py
+++ homework/natalia_aristova/Homework_7/lesson_7_hw_3.py
@@ -3,8 +3,10 @@ b = 'результат операции: 514'
 c = 'результат работы программы: 9'
 d = 'результат: 2'
 
+
 def my_function(stroka):
     print(int(stroka[(stroka.index(':') + 2):]) + 10)
+
 
 my_function(a)
 my_function(b)
