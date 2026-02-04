94cf6889aea93842300bb1be4b56dc6b344e2eb1
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-02-04 19:52:55 +0200

    Aristova N. Homework 7

---
+++ homework/natalia_aristova/Homework_7/lesson_7_hw_2.py
@@ -0,0 +1,4 @@
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+for item in words.items():
+    print(item[0] * item[1])
---
+++ homework/natalia_aristova/Homework_7/lesson_7_hw_3.py
@@ -0,0 +1,12 @@
+a = 'результат операции: 42'
+b = 'результат операции: 514'
+c = 'результат работы программы: 9'
+d = 'результат: 2'
+
+def my_function(stroka):
+    print(int(stroka[(stroka.index(':') + 2):]) + 10)
+
+my_function(a)
+my_function(b)
+my_function(c)
+my_function(d)
