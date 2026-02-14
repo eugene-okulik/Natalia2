5ffdaf7ae210f42583f48d12bf3ba1db6d5d128d
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-02-14 22:31:54 +0200

    Aristova N. Homework 10

---
+++ homework/natalia_aristova/Homework_10/lesson_10_hw_3.py
@@ -0,0 +1,30 @@
+def dec(func):
+
+    def wrapper(first, second):
+        if first < 0 or second < 0:
+             func(first, second, '*')
+        elif first == second:
+             func(first, second, '+')
+        elif first > second:
+             func(first, second, '-')
+        elif first < second:
+             func(first, second, '/')
+    return wrapper
+
+
+@dec
+def calc(first, second, operation):
+    if operation == '+':
+        return first + second
+    elif operation == '-':
+        return first - second
+    elif operation == '*':
+        return first * second
+    elif operation == '/':
+        return first / second
+
+
+a =  int(input('Введите первое число: '))
+b =  int(input('Введите второе число: '))
+
+calc(a, b)
