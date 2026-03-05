b9a166faeceedecbebedd1236badb23e598a6efb
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-03-05 17:07:15 +0200

    Aristova N. Homework 12

--- homework/natalia_aristova/Homework_12/lesson_12_hw.py
+++ homework/natalia_aristova/Homework_12/lesson_12_hw.py
@@ -19,22 +19,22 @@ class Flower:
 
 
 class Rose(Flower):
-    def __init__(self, name, freshness, length, price, lifespan, color):
-        super().__init__(name, freshness, length, price, lifespan)
+    def __init__(self, freshness, length, price, lifespan, color):
+        super().__init__('rose', freshness, length, price, lifespan)
         self.color = color
 
 
 class Peony(Flower):
-    def __init__(self, name, freshness, length, price, lifespan, country):
-        super().__init__(name, freshness, length, price, lifespan)
+    def __init__(self, freshness, length, price, lifespan, country):
+        super().__init__('peony', freshness, length, price, lifespan)
         self.country = country
 
 
-first_rose = Rose('rose', 3, 15, 55, 10, 'red')
-second_rose = Rose('rose', 2, 16, 55, 11, 'white')
-first_peony = Peony('peony', 2, 20, 20, 5, 'Holland')
-second_peony = Peony('peony', 1, 19, 20, 6, 'Greece')
-third_peony = Peony('peony', 4, 21, 20, 3, 'Cyprus')
+first_rose = Rose(3, 15, 55, 10, 'red')
+second_rose = Rose(2, 16, 53, 11, 'white')
+first_peony = Peony(2, 20, 25, 5, 'Holland')
+second_peony = Peony(1, 19, 21, 6, 'Greece')
+third_peony = Peony(4, 21, 56, 3, 'Cyprus')
 
 
 class Bunch:
@@ -43,22 +43,16 @@ class Bunch:
         self.avg_life = self.average_life()
 
     def total_price(self):
-        sum = 0
+        summa = 0
         for i in self.flowers:
-            sum += i.price
-        return sum
+            summa += i.price
+        return summa
 
     def average_life(self):
         return round(sum(i.lifespan for i in self.flowers) / len(self.flowers), 2)
 
-    def sort_by_price(self):
-        return sorted(self.flowers, key=operator.attrgetter('price'))
-
-    def sort_by_length(self):
-        return sorted(self.flowers, key=operator.attrgetter('length'))
-
-    def sort_by_freshness(self):
-        return sorted(self.flowers, key=operator.attrgetter('freshness'))
+    def sort_by(self, key):
+        return sorted(self.flowers, key=operator.attrgetter(key))
 
     def find_by_life(self):
         return [f for f in self.flowers if f.lifespan >= self.avg_life]
@@ -72,7 +66,7 @@ print(third_peony)
 bouquet = Bunch([first_rose, second_rose, first_peony, second_peony, third_peony])
 print(bouquet.total_price())
 print(bouquet.average_life())
-print(bouquet.sort_by_price())
-print(bouquet.sort_by_length())
-print(bouquet.sort_by_freshness())
+print(bouquet.sort_by('price'))
+print(bouquet.sort_by('length'))
+print(bouquet.sort_by('freshness'))
 print(bouquet.find_by_life())
