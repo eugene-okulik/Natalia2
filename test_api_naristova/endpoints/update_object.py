089aa28edc75e27ad6294ee5d8d5ba6ca14dcf9d
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-04-10 23:48:57 +0300

    Aristova N. HW 22

--- test_api_naristova/endpoints/update_object.py
+++ test_api_naristova/endpoints/update_object.py
@@ -8,7 +8,6 @@ class UpdateObject(Endpoint):
     @allure.step('Update an object')
     def make_changes_in_object(self, new_object_id, body, headers=None):
         headers = headers if headers else self.headers
-        self.response = requests.put(f'{self.url}/{new_object_id}',
-                                json=body, headers=headers)
+        self.response = requests.put(f'{self.url}/{new_object_id}', json=body, headers=headers)
         self.json = self.response.json()
         return self.response
