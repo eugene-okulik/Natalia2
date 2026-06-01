70798cfacd884466fa71b39bb112f36b2162a47b
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-06-01 14:45:47 +0300

    Aristova N. HW 30

--- homework/natalia_aristova/Homework_30/lesson_30_hw_1.py
+++ homework/natalia_aristova/Homework_30/lesson_30_hw_1.py
@@ -1,13 +1,13 @@
 from playwright.sync_api import Page, expect, Route
-from time import sleep
 import json
+
 
 def test_change_request(page: Page):
 
     def handle_iphone_name(route: Route):
         response = route.fetch()
         body = response.json()
-        body["body"]["digitalMat"][0]["familyTypes"][0]['productName'] = 'яблокофон 17 про'
+        body['body']['digitalMat'][0]['familyTypes'][0]['productName'] = 'яблокофон 17 про'
         body = json.dumps(body)
         route.fulfill(
             response=response,
@@ -17,6 +17,5 @@ def test_change_request(page: Page):
     page.route('**/shop/api/digital-mat?path=library/step0_iphone/digitalmat&fae=true**', handle_iphone_name)
     page.goto('https://www.apple.com/shop/buy-iphone')
     page.get_by_role('heading', name='iPhone 17 Pro & iPhone 17 Pro Max').click()
-    #sleep(2)
     iphone_header = page.locator('//*[@data-autom="DigitalMat-overlay-header-0-0"]')
     expect(iphone_header).to_have_text('яблокофон 17 про')
